5de15e9fee9b8c937e5a48291cc7872966d6b35a
SWET <swetzadafiya18@gmail.com>
2023-06-22 17:31:25 +0530

    DONE

---
+++ SWET.C
@@ -0,0 +1,16 @@
+// Write a programme to find the minimum number from the given 2 numbers using if else.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int a=8,b=3;
+clrscr();
+if(a<b){
+printf("Minimum value is : %d ",a);
+}else{
+printf("Minimum value is : %d ",b);
+}
+
+getch();
+}
---
+++ SWET2.C
@@ -0,0 +1,19 @@
+// Write a programme to find if a given number is neutral or not using a ladder if else.
+
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int a,b=0,c;
+clrscr();
+printf("Enter any number : %d ",a);
+scanf("%d",&a);
+if(a>b){
+printf("The number is Positive..");
+}else if(a=b){
+printf("The number is Neutral..");
+}else{
+printf("The numer is Positive..");
+}
+
+getch();
+}
